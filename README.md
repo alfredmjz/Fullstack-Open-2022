4e72a54db753233585b393d939dac4b36313a8b0
alfredmjz <alfredmjz@gmail.com>
2022-05-06 19:25:17 -0400

    Reinitialized Repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fullstack-Open-2022
+Submission for Full Stack open 2022
---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,71 @@
+/** @format */
+
+const App = () => {
+	const course = {
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+			},
+		],
+	};
+
+	return (
+		<div>
+			<Header course={course} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+		</div>
+	);
+};
+
+const Header = (props) => {
+	return (
+		<>
+			<h1>{props.course.name}</h1>
+		</>
+	);
+};
+
+const Content = (props) => {
+	return (
+		<div>
+			<Part name={props.parts[0].name} subtotal={props.parts[0].exercises} />
+			<Part name={props.parts[1].name} subtotal={props.parts[1].exercises} />
+			<Part name={props.parts[2].name} subtotal={props.parts[2].exercises} />
+		</div>
+	);
+};
+
+const Total = (props) => {
+	const total =
+		props.parts[0].exercises +
+		props.parts[1].exercises +
+		props.parts[2].exercises;
+
+	return (
+		<>
+			<p>Number of exercises {total}</p>
+		</>
+	);
+};
+
+const Part = (props) => {
+	return (
+		<>
+			<p>
+				{props.name} {props.subtotal}
+			</p>
+		</>
+	);
+};
+export default App;
